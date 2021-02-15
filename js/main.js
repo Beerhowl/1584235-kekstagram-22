a1243be6cd42c5a8b5c2a39af2732df9dfbf6d7e
beer_howl <78041934+Beerhowl@users.noreply.github.com>
2021-02-15 18:09:47 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@ const getRandomNumber = function (min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
-  getRandomNumber(0,9);
-  
-  
+getRandomNumber(0,9);
   const isCheckedString = function (text, maxLength = 140) {
     return text.length <= maxLength;
 }
-
 isCheckedString('Hello world');
